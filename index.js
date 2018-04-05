1f5a7137bcab1a3b32989197c48b014e3eefebdb
Dei Gratia R R <rdeigratia95@gmail.com>
2018-04-05 11:40:43 +0700

    edit js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const showtampil = () => {
     const li = document.createElement("li");
 
     //const tampilNode = document.createTextNode(tampils.nama);
-    li.innerHTML = `${tampils}
+    li.innerHTML = `${tampil.nama}
     <i onclick="destroy(${index})">&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp X</i>
     <i onclick="change(${index})">&nbsp edit</i>`
 
